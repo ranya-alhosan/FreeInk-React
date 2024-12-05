3d69d2ad4cc1fca3336133e257854fb39f658aef
Omarsafii2 <omar.j.safi@gmail.com>
2024-12-05 16:11:45 +0300

    correct the route code

--- blog-backend/routes/api.php
+++ blog-backend/routes/api.php
@@ -25,10 +25,8 @@ Route::post('/login', [App\Http\Controllers\Api\ApiController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', [App\Http\Controllers\Api\ApiController::class, 'profile']);
-    Route::put('/updateuuuuuuu', [App\Http\Controllers\Api\ApiController::class, 'updateProfileuuuuuu'] );
     Route::get('/logout', [App\Http\Controllers\Api\ApiController::class, 'logout']);
     Route::put('/update', [App\Http\Controllers\Api\ApiController::class, 'updateProfile'] );
-    Route::put('/updatekijhwdkf', [App\Http\Controllers\Api\ApiController::class, 'updateProfilesdfjlhsdf'] );
     
 });
 
