5aee07eed0fe430147eed9c8e4db72e762e9424b
Omarsafii2 <omar.j.safi@gmail.com>
2024-12-05 13:36:55 +0300

    v1 backend

--- blog-backend/routes/api.php
+++ blog-backend/routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,13 +14,6 @@ use App\Http\Controllers\API\PostController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::resource('posts', PostController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
